8b89286b8f50206450dae482b28d3d80409893ed
Beyley Cardellio <ep1cm1n10n123@gmail.com>
2025-08-02 21:09:09 -0700

    Update build to latest zig master

--- build.zig
+++ build.zig
@@ -15,32 +15,22 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "mailbox",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/mailbox.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
     _ = b.addModule("mailbox", .{
         .root_source_file = b.path("src/mailbox.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const unit_tests_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .root_source_file = b.path("src/mailbox_tests.zig"),
+    });
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/mailbox_tests.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = unit_tests_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
